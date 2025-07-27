852b6f3a2f4296aee6d6737590652db10e0c717f
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-27 22:57:50 +0600

    I created an AuthProvider.jsx and created an AuthContext with default value null and then wrap the whole routerProvider inside the AuthContext and then back to the AuthContext.jsx and then use children

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './Layout/Root';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import { AuthContext } from './AuthProvider/AuthProvider';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root></Root>,
     children: [
       {
         path: '/',
-        element: <Home></Home>
+        element: <Home></Home>,
       },
       {
         path: 'login',
-        element: <Login></Login>
+        element: <Login></Login>,
       },
       {
         path: 'register',
-        element: <Register></Register>
-      }
-    ]
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <AuthContext>
+      <RouterProvider router={router} />
+    </AuthContext>
+  </StrictMode>
+);
