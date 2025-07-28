a642ec8481db94e5d00f13649831f1fbc7631eb2
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-28 21:58:56 +0600

    a function is created into the AuthProvider.jsx for signInUser with 2 peramiters email, password then return this hook signInWithEmailAndPassword(auth, email, password); and then I access the AuthContext from login.jsx via useContext(AuthContext) hook and then I call the function with peramiter and then console log and catch error, so people can sign in now

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { createContext } from 'react';
 import { auth } from '../firebase.init';
 
@@ -10,9 +10,14 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
   const authInfo = {
     name: 'sagor nodi',
-    createUser
+    createUser,
+    signInUser
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const Login = () => {
+  const { signInUser } = useContext(AuthContext);
 
-  const handleLogin = e =>{
+  const handleLogin = (e) => {
     e.preventDefault();
-    const email= e.target.email.value;
-    const password= e.target.password.value;
-    console.log(email, password)
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
 
-  }
+    // login a user
+    signInUser(email, password)
+      .then((result) => {
+        console.log('user logged in succefully', result.user);
+      })
+      .catch((error) => {
+        console.log('Error', error.message);
+      });
+  };
 
   return (
-    <div className='flex justify-center items-center'>
+    <div className="flex justify-center items-center">
       <div className=" flex-col">
         <div className="text-center">
           <h1 className="text-2xl font-bold">Login now</h1>
@@ -21,15 +31,34 @@ const Login = () => {
           <div className="card-body">
             <form onSubmit={handleLogin} className="">
               <label className="label text-base">Email</label>
-              <input type="email" name="email" className="input mb-4" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                className="input mb-4"
+                placeholder="Email"
+              />
               <label className="label text-base">Password</label>
-              <input type="password" name="password" className="input mb-4" placeholder="Password" />
+              <input
+                type="password"
+                name="password"
+                className="input mb-4"
+                placeholder="Password"
+              />
               <div>
-                <Link className='text-base label hover:text-blue-500'>Forgot password?</Link>
+                <Link className="text-base label hover:text-blue-500">
+                  Forgot password?
+                </Link>
               </div>
-              <button className="btn bg-blue-500 text-white block mx-auto w-full text-center text-base font-normal mt-4">Login</button>
+              <button className="btn bg-blue-500 text-white block mx-auto w-full text-center text-base font-normal mt-4">
+                Login
+              </button>
             </form>
-            <p className='text-base'>Dont' have an account? <Link to="/register" className='text-base hover:text-blue-500'>Register</Link></p>
+            <p className="text-base">
+              Dont' have an account?{' '}
+              <Link to="/register" className="text-base hover:text-blue-500">
+                Register
+              </Link>
+            </p>
           </div>
         </div>
       </div>
