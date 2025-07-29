a332d071f7e72bca274033da22394b35de6af62c
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-29 11:31:17 +0600

    useNavigate() hook imported in register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const Register = () => {
   const { createUser } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  ;
 
   const handleRegister = (e) => {
     e.preventDefault();
